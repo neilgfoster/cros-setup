66782b62dddad0b04d3a0d280a38efc5d60844fe
neilgfoster <1370457+neilgfoster@users.noreply.github.com>
2025-09-23 22:06:18 +0100

    Refactor setup scripts: remove flatpak and dark mode scripts, update README for clarity

--- .setup/scripts/001-flatpak.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Error handling
-set -e
-
-# Install flatpak and flathub
-sudo apt install -y flatpak
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
